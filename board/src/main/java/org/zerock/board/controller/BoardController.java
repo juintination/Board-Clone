11acf144aae48807a6b8d04e6f6af3624cd65b78
DEOKJAE KWON <juintin@kakao.com>
2024-02-24 17:33:32 +0900

    feat: add register logic

--- board/src/main/java/org/zerock/board/controller/BoardController.java
+++ board/src/main/java/org/zerock/board/controller/BoardController.java
@@ -5,7 +5,10 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.board.dto.BoardDTO;
 import org.zerock.board.dto.PageRequestDTO;
 import org.zerock.board.service.BoardService;
 
@@ -21,5 +24,19 @@ public class BoardController {
     public void list(PageRequestDTO pageRequestDTO, Model model) {
         log.info("list............." + pageRequestDTO);
         model.addAttribute("result", boardService.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register() {
+        log.info("register get...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(BoardDTO dto, RedirectAttributes redirectAttributes) {
+        log.info("dto..." + dto);
+        Long bno = boardService.register(dto);
+        log.info("BNO: " + bno);
+        redirectAttributes.addFlashAttribute("msg", bno);
+        return "redirect:/board/list";
     }
 }
