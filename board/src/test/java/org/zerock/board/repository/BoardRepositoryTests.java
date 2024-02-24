2febf65449ce1b262d9152417d0b993d5f782d4e
DEOKJAE KWON <juintin@kakao.com>
2024-02-24 18:42:09 +0900

    test: modify to print the result of the testSearchPage

--- board/src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
+++ board/src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
@@ -81,6 +81,10 @@ public class BoardRepositoryTests {
     public void testSearchPage() {
         Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending()
                 .and(Sort.by("title").ascending()));
-        Page<Object[]> result = boardRepository.searchPage("t", "1", pageable);
+        Page<Object[]> result = boardRepository.searchPage("w", "55", pageable);
+        result.get().forEach(row -> {
+            Object[] arr = (Object[])row;
+            System.out.println(Arrays.toString(arr));
+        });
     }
 }
